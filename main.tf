1c63f446d116126f8b8d8fd2000d0fbbb7d15522
Mihai Militaru <militaru.mihai.cristian@gmail.com>
2025-06-23 18:11:56 +0300

    Corrected ami

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "Test_Terraform_EC2" {
-  ami = "ami-042b4708b1d05f512"   #Ubuntu 24.04
+  ami = "ami-020cba7c55df1f615"   #Ubuntu 24.04
   instance_type = "t3.micro"
 
   tags = {
