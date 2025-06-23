d7d56fe20d5673c966f05c6c95102d70af94af27
Mihai Militaru <militaru.mihai.cristian@gmail.com>
2025-06-23 18:31:40 +0300

    Added second instance

--- main.tf
+++ main.tf
@@ -2,11 +2,20 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "Test_Terraform_EC2" {
-  ami = "ami-020cba7c55df1f615"   #Ubuntu 24.04
+resource "aws_instance" "instance1" {
+  ami = "ami-020cba7c55df1f615"   # Ubuntu 24.04
   instance_type = "t3.micro"
 
   tags = {
-    Name = "Terraform_Tag"
+    Name = "EC2_instance1"   # Instance name
+  }
+}
+
+resource "aws_instance" "instance2" {
+  ami = "ami-020cba7c55df1f615"   # Ubuntu 24.04
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "EC2_Instance2"   # Instance name
   }
 }
