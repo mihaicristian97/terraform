ec1658d22236e0502867711629211fdab0b7d692
Mihai Militaru <militaru.mihai.cristian@gmail.com>
2025-06-23 18:04:50 +0300

    Terraform code for ubuntu EC2 instance

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_instance" "Test_Terraform_EC2" {
+  ami = "ami-042b4708b1d05f512"   #Ubuntu 24.04
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Terraform_Tag"
+  }
+}
